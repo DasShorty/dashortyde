87738a7f169aabdd6447ebac350e6055ab2c9307
dasshorty <anthony.timmel@dasshorty.de>
2024-06-26 13:13:04 +0200

    update

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+   const footerYear = document.getElementById("footer-year");
+
+   footerYear.innerText = new Date().getFullYear() + " COPYRIGHT"
+
+});
